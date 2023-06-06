1c9ab2b903545c8cf56c8d8fae96e3cbaa0ea0d3
Diego <diegodac77@gmail.com>
2023-06-06 19:53:15 -0300

    commit 11

--- client/src/components/CardsContainer/CardsContainer.jsx
+++ client/src/components/CardsContainer/CardsContainer.jsx
@@ -12,7 +12,7 @@ const CardsContainer = () => {
         return (
           <Card
             key={pokemon.id}
-            image={!pokemon.InDb ? pokemon.image + " " : pokemon.img + " "}
+            image={!pokemon.InDb ? pokemon.imagen + " " : pokemon.img + " "}
             name={pokemon.name}
             types={!pokemon.InDb ? pokemon.types + " " : pokemon.Types + " "}
           />
